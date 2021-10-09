6f75d482c412039f33d626386bdee8a4152a441d
LineWelder <redalerm@gmail.com>
2021-10-09 11:24:22 +0200

    Created basic token types

---
+++ CompilerLibrary/Tokenizing/Token.cs
@@ -0,0 +1,25 @@
+﻿namespace CompilerLibrary.Tokenizing
+{
+    public enum TokenType
+    {
+        Identifier,
+        IntegerLiteral,
+        Equals,
+        Semicolon
+    }
+
+    /// <summary>
+    /// Represents a simple token
+    /// </summary>
+    public record Token(TokenType Type);
+
+    /// <summary>
+    /// Represents a token containing an integer value
+    /// </summary>
+    public record IntegerToken(TokenType Type, int Value) : Token(Type);
+
+    /// <summary>
+    /// Represents a token containing a string value
+    /// </summary>
+    public record StringToken(TokenType Type, string Value) : Token(Type);
+}
--- CompilerLibrary/Tokenizing/Tokenizer.cs
+++ CompilerLibrary/Tokenizing/Tokenizer.cs
@@ -1,19 +1,34 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace CompilerLibrary.Tokenizing
 {
+    /// <summary>
+    /// Is used for splitting the source code into tokens
+    /// </summary>
     public class Tokenizer
     {
-        public StreamReader stream;
+        private readonly Dictionary<char, TokenType> SYMBOLS = new()
+        {
+            { '=', TokenType.Equals },
+            { ';', TokenType.Semicolon }
+        };
 
+        private readonly StreamReader stream;
+
+        /// <param name="stream">The stream the tokens will be read from</param>
         public Tokenizer(StreamReader stream)
         {
             this.stream = stream;
         }
 
-        public string NextToken()
+        /// <summary>
+        /// Consumes the next token
+        /// </summary>
+        /// <returns>The token</returns>
+        public Token NextToken()
         {
-            return stream.ReadLine();
+            throw new System.NotImplementedException();
         }
     }
 }

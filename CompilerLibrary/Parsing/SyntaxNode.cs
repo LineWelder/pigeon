223969e1024b78b7f2502dfc67bea2fd09e3d45a
LineWelder <redalerm@gmail.com>
2021-10-09 15:09:11 +0200

    Removed SyntaxNodeType enum

--- CompilerLibrary/Parsing/SyntaxNode.cs
+++ CompilerLibrary/Parsing/SyntaxNode.cs
@@ -1,22 +1,23 @@
 ﻿namespace CompilerLibrary.Parsing
 {
-    public enum SyntaxNodeType
-    {
-        VariableDeclaration,
-        Identifier
-    }
-
     /// <summary>
     /// Represents a simple syntax node
     /// </summary>
-    public record SyntaxNode(Location Location, SyntaxNodeType Type);
+    public record SyntaxNode(Location Location);
 
     /// <summary>
     /// Represents a variable declaration
     /// <VariableType> <Identifier> = <Value>;
     /// </summary>
     public record VariableDeclarationNode(
-        Location Location, SyntaxNodeType Type,
+        Location Location,
         SyntaxNode VariableType, string Identifier, SyntaxNode Value
-    ) : SyntaxNode(Location, Type);
+    ) : SyntaxNode(Location);
+
+    /// <summary>
+    /// Represents an identifier
+    /// </summary>
+    public record IdentifierNode(
+        Location Location, string Value
+    ) : SyntaxNode(Location);
 }

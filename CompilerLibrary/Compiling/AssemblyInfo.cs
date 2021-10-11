45cf7f8b5aa8b1363725844ca2200c71d3be3267
LineWelder <redalerm@gmail.com>
2021-10-11 14:23:42 +0200

    Updated CompilerLibrary assembly info and added signature to generated FASM code

---
+++ CompilerLibrary/Compiling/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Pigeon compiler")]
+[assembly: AssemblyCompany("LineWelder")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]

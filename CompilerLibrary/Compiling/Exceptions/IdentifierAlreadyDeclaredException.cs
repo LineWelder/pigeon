5322cb0b057d73248a7fd171cb5bf89353e5755a
LineWelder <redalerm@gmail.com>
2022-03-21 20:25:52 +0100

    Added IdentifierAlreadyDeclaredException

---
+++ CompilerLibrary/Compiling/Exceptions/IdentifierAlreadyDeclaredException.cs
@@ -0,0 +1,31 @@
+﻿using CompilerLibrary.Parsing;
+using System;
+
+namespace CompilerLibrary.Compiling.Exceptions;
+
+public class IdentifierAlreadyDeclaredException : CompilerException
+{
+    public SyntaxNode Redeclaration { get; init; }
+    public Location PreviousDeclaration { get; init; }
+
+    public string Identifier => GetIdentifier(Redeclaration);
+
+    public IdentifierAlreadyDeclaredException(
+        SyntaxNode redeclaration, Location previousDeclaration)
+        : base(
+            redeclaration.Location,
+            $"Identifier {GetIdentifier(redeclaration)} already declared in {previousDeclaration}"
+        )
+    {
+        Redeclaration = redeclaration;
+        PreviousDeclaration = previousDeclaration;
+    }
+
+    private static string GetIdentifier(SyntaxNode node)
+        => node switch
+        {
+            VariableDeclarationNode variable => variable.Identifier,
+            FunctionDeclarationNode function => function.Identifier,
+            _ => throw new Exception("Unexpected type")
+        };
+}
